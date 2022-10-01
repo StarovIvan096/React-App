c6374b51634d4f04dd95a436518d07165e62165b
Ivan Starov <pp24rsk@gmail.com>
2022-10-01 23:07:43 +0200

    дополнительно разбил компоненту Profile на такие состовляющие как: активность поста, мой комментарий, отправленные комменатрии. Так же создал отдельные папки для этих компонентов и поместил туда стили этих компонентов, поработал с props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import bodyImg from './images/home.jpg'
 import './App.css';
-import Header from './modules/Header';
-import WrappContentSite from './modules/Wrapper';
+import Header from './Header/Header';
+import WrappContentSite from './Wrapper';
 const App = ()=> {
   return (
     <div className="all-write">
--- src/modules/Footer.js
+++
@@ -1,7 +0,0 @@
-const Footer = ()=> {
-    return(
-      <div className="footer-site">Footer</div>
-    )
-}
-
-export default Footer;
